cfa27687526beee07e75bdfd7cf912d7f77b26ae
kiadso <genelledenzin@cohhio.org>
2020-09-03 13:59:13 -0400

    getting WhatToCheck ready to use

--- draft 3.R
+++ draft 3.R
@@ -13,7 +13,6 @@ census_full <-
     return_type = 'geographies'
   ) 
 
-if(nrow(census_full > 1)) {
   refinements <- census_full %>%
     select(singlelineaddress, matchedAddress, starts_with("addressComponents")) %>%
     mutate(DifferentStreetName = if_else(length(unique(addressComponents.streetName)) > 1,
@@ -29,19 +28,22 @@ if(nrow(census_full > 1)) {
            DifferentState = if_else(length(unique(addressComponents.state)) > 1,
                                     "State", NULL),
            DifferentZIP = if_else(length(unique(addressComponents.zip)) > 1,
-                                  "ZIP", NULL),
+                                  "ZIP Code", NULL),
            WhatToCheck = paste("Please check your", 
-                               DifferentStreetName,
+                               na.omit(DifferentStreetName),
                                DifferentStreetSuffix,
                                DifferentSuffixQualifier,
                                DifferentSuffixDirection,
                                DifferentCity, 
                                DifferentState,
                                DifferentZIP)
-           )
-}
+           ) %>%
+    pull(WhatToCheck) %>%
+    unique()
 
-tracts_df <- as.data.frame(census_full$`geographies.Census Tracts`)
+tracts_df <- if(refinements == "Please check your NA NA NA NA NA NA NA") {
+  as.data.frame(census_full$`geographies.Census Tracts`)
+  }
 
 tract_no <- tracts_df %>%
   pull(GEOID)
