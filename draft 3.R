47f64e4f0d2687bd1555f594937f7c54aa634725
kiadso <genelledenzin@cohhio.org>
2020-09-03 12:44:06 -0400

    added a way of dealing with it when you get multiple rows in census_full

--- draft 3.R
+++ draft 3.R
@@ -1,21 +1,48 @@
 library(tibble)
-library(DT)
 library(dplyr)
 library(tidygeocoder)
 
-address_single2 <- tibble(singlelineaddress = "605 N High St, Columbus, Ohio 43215")
+address <- tibble(singlelineaddress = "6576 Borr Ave, Columbus, OH 43068")
 
-census_full1 <-
-  address_single2 %>%
+census_full <-
+  address %>%
   geocode(
     address = singlelineaddress,
     method = 'census',
     full_results = TRUE,
     return_type = 'geographies'
-  )
+  ) 
 
-tracts_df <- as.data.frame(census_full1$`geographies.Census Tracts`)
+if(nrow(census_full > 1)) {
+  refinements <- census_full %>%
+    select(singlelineaddress, matchedAddress, starts_with("addressComponents")) %>%
+    mutate(DifferentStreetName = if_else(length(unique(addressComponents.streetName)) > 1,
+                                         "Street Name", NULL),
+           DifferentStreetSuffix = if_else(length(unique(addressComponents.suffixType)) > 1,
+                                          "Street Suffix (for example: Ave, Street, Way)", NULL),
+           DifferentSuffixDirection = if_else(length(unique(addressComponents.suffixDirection)) > 1,
+                                              "Street Direction (for example: N, W)", NULL),
+           DifferentSuffixQualifier = if_else(length(unique(addressComponents.suffixQualifier)) > 1,
+                                              "Suffix Qualifier", NULL),
+           DifferentCity = if_else(length(unique(addressComponents.city)) > 1,
+                                   "City", NULL),
+           DifferentState = if_else(length(unique(addressComponents.state)) > 1,
+                                    "State", NULL),
+           DifferentZIP = if_else(length(unique(addressComponents.zip)) > 1,
+                                  "ZIP", NULL),
+           WhatToCheck = paste("Please check your", 
+                               DifferentStreetName,
+                               DifferentStreetSuffix,
+                               DifferentSuffixQualifier,
+                               DifferentSuffixDirection,
+                               DifferentCity, 
+                               DifferentState,
+                               DifferentZIP)
+           )
+}
 
-blocks_df <- as.data.frame(census_full1$`geographies.2010 Census Blocks`)
+tracts_df <- as.data.frame(census_full$`geographies.Census Tracts`)
 
-glimpse(census_full1)
+tract_no <- tracts_df %>%
+  pull(GEOID)
+
